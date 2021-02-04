bd5b6f07051a559c55b035bb5084a98f7e72b698
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2021-02-04 10:54:56 -0500

    SimpleDB

---
+++ Src/Modules/SimpleDB/SimpleDB.cs
@@ -0,0 +1,36 @@
+﻿using RT.PropellerApi;
+using RT.Servers;
+using RT.Json;
+
+namespace RT.Propeller.Modules
+{
+    class SimpleDB : PropellerModuleBase<SimpleDBSettings>
+    {
+        public int count = 0;
+
+        public override string Name { get { return "SimpleDB"; } }
+
+        public override HttpResponse Handle(HttpRequest req)
+        {
+            if (req.Headers.ContainsKey("SIMPLEDBADD"))
+                if (req.Headers["SIMPLEDBADD"] == "TRUE")
+                    count++;
+
+            if (req.Headers.ContainsKey("SIMPLEDBSUB"))
+                if (req.Headers["SIMPLEDBSUB"] == "TRUE")
+                    count--;
+            if (count < 0)
+                count = 0;
+
+            var dict = new JsonDict();
+            dict.Add("Count", JsonNumber.Create(count));
+            return HttpResponse.Json(dict);
+        }
+    }
+
+    /// <summary>Contains the settings for the SimpleDB Propeller module.</summary>
+    public sealed class SimpleDBSettings
+    {
+        //Nothing here yet.
+    }
+}
